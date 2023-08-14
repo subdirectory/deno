a2242b4d01334fd2d7d2c25fc111ac5ec5a905c0
navigaid <navigaid@gmail.com>
2023-08-14 19:04:20 +0000

    change prompt character to ❯

--- cli/tools/repl/editor.rs
+++ cli/tools/repl/editor.rs
@@ -479,7 +479,7 @@ impl ReplEditor {
   }
 
   pub fn readline(&self) -> Result<String, ReadlineError> {
-    self.inner.lock().readline("> ")
+    self.inner.lock().readline("❯ ")
   }
 
   pub fn update_history(&self, entry: String) {
